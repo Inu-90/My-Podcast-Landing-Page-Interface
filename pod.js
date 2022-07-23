7fc2824502ac1333e2df8a3aeffe8a1974526e8c
Inu-90 <ismailinu49@gmail.com>
2022-07-23 16:04:57 +0100

    modified

--- pod.js
+++ pod.js
@@ -1,14 +1,31 @@
-var emailAddress = document.getElementById('email').value;
-var reqAccess = document.getElementById('access');
 
-reqAccess.addEventListener("click", function() {
-        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    
-        if (!filter.test(email.value)) {
-        alert('Please provide a valid email address');
-        return false;
-     } else {
-         alert('Valid email');
-     }
-    }
-    );
+//Selecetors
+let emailAddress = document.getElementById("email").value;
+let reqAccess = document.getElementById("access");
+let alert1 = document.querySelector(".alert1");
+let alert2 = document.querySelector(".alert2");
+let alert_ok = document.querySelector(".ok");
+let cancel = document.querySelector(".cancel");
+let yes = document.querySelector("yes");
+
+reqAccess.addEventListener("click", function () {
+  let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  if (!filter.test(email.value)) {
+    alert1.style.display = "flex";
+
+  } else if (filter.test(email.value))
+    alert2.style.display = "flex";
+});
+
+alert_ok.addEventListener("click", () => {
+    alert1.style.display = "none";
+});
+
+cancel.addEventListener("click", () => {
+  alert2.style.display = "none";
+});
+
+yes.addEventListener("click" , () => {
+  alert2.style.display = "none";
+})
